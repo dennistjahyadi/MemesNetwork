384ae3859ddf9311cf91ebaaeb187804e56cd6f0
Dennis Tjahyadi <dennis.tgt@gmail.com>
2019-02-23 22:28:32 +0700

    sadf

--- app/src/main/java/com/example/acer/memesnetwork/utils/Utils.java
+++ app/src/main/java/com/example/acer/memesnetwork/utils/Utils.java
@@ -1,8 +1,13 @@
 package com.example.acer.memesnetwork.utils;
 
+import android.content.res.AssetFileDescriptor;
+
+import java.util.List;
+
 public class Utils {
 
-    public static final String DOMAIN = "http://192.168.1.8:8000";
+    public static final String DOMAIN = "http://192.168.100.145:8000";
     public static final String API_URL = DOMAIN+"/api/";
     public static final String SOURCE_URL = DOMAIN+"/sources/";
+
 }
