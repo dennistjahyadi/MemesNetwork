b83ea8b7af39ee5e7394bffe3936e23f64a80861
Dennis Tjahyadi <dennis.tgt@gmail.com>
2019-02-24 02:22:14 +0700

    sadf

--- app/src/main/java/com/example/acer/memesnetwork/utils/Utils.java
+++ app/src/main/java/com/example/acer/memesnetwork/utils/Utils.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class Utils {
 
-    public static final String DOMAIN = "http://192.168.100.145:8000";
+    public static final String DOMAIN = "http://192.168.1.8:8000";
     public static final String API_URL = DOMAIN+"/api/";
     public static final String SOURCE_URL = DOMAIN+"/sources/";
 
--- video-player-manager/src/main/java/com/volokh/danylo/video_player_manager/Helper.java
+++ video-player-manager/src/main/java/com/volokh/danylo/video_player_manager/Helper.java
@@ -6,5 +6,4 @@ import java.util.List;
 
 public class Helper {
     public static List<AssetFileDescriptor> videoCache;
-
 }
